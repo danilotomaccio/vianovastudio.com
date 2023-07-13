d05f7ea07b04a71b024fda5c92aa364bbb36a159
danilotomaccio <danilo.tomaccio@gmail.com>
2023-07-13 23:31:39 +0200

    Add GSAP plugins

--- main.js
+++ main.js
@@ -26,3 +26,34 @@ gsap.to('.letter', {
     delay: 0.2,
     duration: .1
 })
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollTrigger.normalizeScroll(true);
+
+let smoother = ScrollSmoother.create({
+    smooth: 2,
+    effects: true,
+    normalizeScroll: true
+  });
+  
+  // pin box-c when it reaches the center of the viewport, for 300px
+  ScrollTrigger.create({
+    trigger: ".box-c",
+    pin: true,
+    start: "center center",
+    end: "+=300",
+    markers: true
+  });
+  
+  document.querySelector("button").addEventListener("click", e => {
+    // scroll to the spot where .box-c is in the center.
+    // parameters: element, smooth, position
+    smoother.scrollTo(".box-c", true, "center center");
+    
+    // or you could animate the scrollTop:
+    // gsap.to(smoother, {
+    // 	scrollTop: smoother.offset(".box-c", "center center"),
+    // 	duration: 1
+    // });
+  });
+  
