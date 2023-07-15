4b048688ec4127589fa8078ba465f9d91017ac6e
danilotomaccio <danilo.tomaccio@gmail.com>
2023-07-15 02:39:39 +0200

    Boh

--- main.js
+++ main.js
@@ -1,59 +1,49 @@
-const myText = document.querySelector('#my-text');
-const text = myText.textContent;
-myText.textContent = '';
-
-for (let letter of text) {
-    const div = document.createElement('div');
-    div.className = 'letter-container';
-    div.style = "display: inline-block; translate: none; rotate: none; scale: none; transform: translate(0px, 0px);";
-
-    let letterElement = document.createElement('div');
-    letterElement.textContent = letter;
-    const fontSizeValue = ((Math.random() * 100) + 50);
-    const fontSize = fontSizeValue + "px";
-    letterElement.style.fontSize = fontSize;
-    letterElement.style.lineHeight = fontSize;
-    // letterElement.style.transform = `translateY(${-fontSizeValue - 115}px)`;
-    letterElement.className = 'letter';
-
-    div.appendChild(letterElement);
-    myText.appendChild(div);
-}
-
-gsap.to('.letter', {
-    y: 0,
-    stagger: 0.05,
-    delay: 0.2,
-    duration: .1
-})
-
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollTrigger.normalizeScroll(true);
 
 let smoother = ScrollSmoother.create({
-    smooth: 2,
-    effects: true,
-    normalizeScroll: true
-  });
-  
-  // pin box-c when it reaches the center of the viewport, for 300px
-  ScrollTrigger.create({
-    trigger: ".box-c",
+  smooth: 2,
+  effects: true,
+  normalizeScroll: true
+});
+
+ScrollTrigger.create({
+  trigger: "#sez-1",
+  pin: true,
+  // start: "center center",
+  end: "+=800",
+  // markers: true
+});
+
+ScrollTrigger.create({
+  trigger: "#sez-2",
+  pin: true,
+  // start: "center center",
+  end: "+=800",
+  // markers: true
+});
+
+gsap.to(document.getElementById("cutter"), {
+  xPercent: 100,
+  ease: 'none',
+  scrollTrigger: {
+    trigger: "#sez-3",
     pin: true,
-    start: "center center",
-    end: "+=300",
-    markers: true
-  });
+    scrub: 1,
+    end: "+=1000",
+    //markers: true
+  }
+}
+);
+
+/* document.querySelector("button").addEventListener("click", e => {
+  // scroll to the spot where .box-c is in the center.
+  // parameters: element, smooth, position
+  smoother.scrollTo(".box-c", true, "center center");
   
-  document.querySelector("button").addEventListener("click", e => {
-    // scroll to the spot where .box-c is in the center.
-    // parameters: element, smooth, position
-    smoother.scrollTo(".box-c", true, "center center");
-    
-    // or you could animate the scrollTop:
-    // gsap.to(smoother, {
-    // 	scrollTop: smoother.offset(".box-c", "center center"),
-    // 	duration: 1
-    // });
-  });
-  
+  // or you could animate the scrollTop:
+  // gsap.to(smoother, {
+  // 	scrollTop: smoother.offset(".box-c", "center center"),
+  // 	duration: 1
+  // });
+}); */
