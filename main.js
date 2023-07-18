846368956d7aa4a2aec17ffbfa9e82681bff06c5
danilotomaccio <danilo.tomaccio@gmail.com>
2023-07-18 08:25:44 +0200

    Boh

--- main.js
+++ main.js
@@ -23,18 +23,31 @@ ScrollTrigger.create({
   // markers: true
 });
 
-gsap.to(document.getElementById("cutter"), {
-  xPercent: 100,
+const cutterTimeline = new gsap.timeline({
   ease: 'none',
-  scrollTrigger: {
+  /* scrollTrigger: {
     trigger: "#sez-3",
     pin: true,
     scrub: 1,
-    end: "+=1000",
+    end: `+=800`,
+    // end: `+=${window.innerHeight * 2}`,
     //markers: true
-  }
-}
-);
+  } */
+});
+
+/* cutterTimeline.fromTo(document.getElementById("cutter"),
+  {
+    xPercent: -100
+  },
+  {
+    xPercent: 100,
+  },
+  1.5
+); */
+
+cutterTimeline.to("#cutter", {
+  xPercent: 100
+})
 
 /* document.querySelector("button").addEventListener("click", e => {
   // scroll to the spot where .box-c is in the center.
@@ -47,3 +60,47 @@ gsap.to(document.getElementById("cutter"), {
   // 	duration: 1
   // });
 }); */
+
+
+
+
+/* let cv_tl = new gsap.timeline({
+  ease: 'none',
+  scrollTrigger: {
+    trigger: "#cont_cv",
+    start: "top top+=100",
+    end: "+=800",
+    scrub: true,
+    markers: true,
+    pin: true
+  }
+}); */
+
+gsap.set("#cont_cv", { perspective: 1000 });
+
+
+cutterTimeline.from(
+  "#cont_cv .top_cv",
+  {
+    duration: 2,
+    transformOrigin: "left top",
+    autoAlpha: 0,
+    ease: "none"
+    //transformStyle: "preserve3d",
+  },
+  1.5
+);
+
+cutterTimeline.from(
+  "#cont_cv .not_top_cv",
+  {
+    duration: 2,
+    transformOrigin: "left top",
+    autoAlpha: 0,
+    ease: "none",
+    //transformStyle: "preserve3d",
+    rotationX: 180, // do not use negative rotation in this case
+    stagger: 3
+  },
+  "+1.5"
+);
